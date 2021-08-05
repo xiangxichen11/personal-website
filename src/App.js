97c70d462b22a45f11d8bb08563935ba6d06537a
Xiang Xi Chen <xiangxi.chen.ca@gmail.com>
2021-08-04 20:51:14 -0600

    Changed HomePage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi, I'm Xiang Xi
-        </a>
-      </header>
+      <body className="App-body">
+        <h1>Hi, I'm XiangXi</h1>
+          <div>
+            <img src="https://st1.bollywoodlife.com/wp-content/uploads/2020/11/Jungkook-BTS.jpg" class="Jungkook" alt="Jungkook" ></img>
+          </div>
+
+      </body>
+
+        
     </div>
   );
 }
