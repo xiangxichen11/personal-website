11590739850e620b4f423d4f43c471e0b37a935f
Xiang Xi Chen <xiangxi.chen.ca@gmail.com>
2021-08-09 21:07:49 -0600

    Added typewriter animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import "./Home.css";
+
 
 export default function Home(){
     return (
         <main>
-            <img/>
-            <section>
-                <h1>Hi, I'm XiangXi</h1>
+            <img src="https://st1.bollywoodlife.com/wp-content/uploads/2020/11/Jungkook-BTS.jpg" alt="Jungkook" className="Profile-Photo" />
+            <section >
+                <div className="hi">
+                    <p>Hi!</p>
+                </div>
+                <div className="typewriter">
+                        <p>I'm Xiang Xi</p>
+                </div>
+                
+
             </section>
         </main>
     )
