16121aab057d16788d619b3b9779f643d68abb4f
Xiang Xi Chen <xiangxi.chen.ca@gmail.com>
2021-08-09 22:59:46 -0600

    Changed homepage layout

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import "./Home.css";
+import image from "./Images/background.jpg"
 
 
 export default function Home(){
     return (
         <main>
-            <img src="https://st1.bollywoodlife.com/wp-content/uploads/2020/11/Jungkook-BTS.jpg" alt="Jungkook" className="Profile-Photo" />
-            <section >
-                <div className="hi">
-                    <p>Hi!</p>
-                </div>
-                <div className="typewriter">
-                        <p>I'm Xiang Xi</p>
+            <div>
+                <img src="https://st1.bollywoodlife.com/wp-content/uploads/2020/11/Jungkook-BTS.jpg" alt="Jungkook" className="Profile-Photo" />
+            </div>
+
+            <section>
+                <div className="banner">
+                    <div className="hi">
+                        <h1>Hi!</h1>
+                    </div>
+                    <div className="typewriter">
+                            <h2>I'm Xiang Xi. Glad you could make it!</h2>
+                    </div>
                 </div>
                 
 
