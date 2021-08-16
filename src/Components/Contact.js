166f6dbb1ece9c7f65fd07005a3e2fcc29d756a5
Anton Chen <contact@antonchen.ca>
2021-08-16 16:10:52 -0600

    Fix formatting

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,14 +4,16 @@ export default function Contact() {
   document.title = "XiangXiChen/Contact";
 
   return (
-    <div>
-        <h1>Contact page</h1>
-    </div>
-    <div>
-        <h2>Add me on ig @xiangxichen</h2>
-    </div>
-    <div>
-        <img src="http://images6.fanpop.com/image/photos/39400000/My-Club-Needed-More-RapMon-kimchis-center-39466209-711-853.jpg" alt="RM" className="Photo"/>
-    </div>
+    <main>
+      <div>
+          <title>Contact page</h1>
+      </div>
+      <div>
+          <h2>Add me on ig @xiangxichen</h2>
+      </div>
+      <div>
+          <img src="http://images6.fanpop.com/image/photos/39400000/My-Club-Needed-More-RapMon-kimchis-center-39466209-711-853.jpg" alt="RM" className="Photo"/>
+      </div>
+    </main>
   )
 }
