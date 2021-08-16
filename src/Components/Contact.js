8e210759943b237d6418ec7fe33e89703342ec17
Anton Chen <contact@antonchen.ca>
2021-08-16 15:59:29 -0600

    Add self portrait and contact information

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-export default function Contact(){
-    document.title = "XiangXiChen/Contact";
+export default function Contact() {
+  document.title = "XiangXiChen/Contact";
 
-    return <h1>Contact page</h1>
+  return (
+    <div>
+        <h1>Contact page</h1>
+    </div>
+    <div>
+        <h2>Add me on ig @xiangxichen</h2>
+    </div>
+    <div>
+        <img src="http://images6.fanpop.com/image/photos/39400000/My-Club-Needed-More-RapMon-kimchis-center-39466209-711-853.jpg" alt="RM" className="Photo"/>
+    </div>
+  )
 }
