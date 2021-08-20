1d492983b0b9c2e6a67af972bafc9af00b46d2aa
xiangxichen11 <xiangxi.chen.ca@gmail.com>
2021-08-19 22:01:10 -0600

    Changed up Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,20 +6,21 @@ export default function Navbar() {
     return (
         <header className="Nav-header">
             <div className="Nav-div">
-                <nav className="Navbar">
+                <nav className="Nav-left">
                     <NavLink to="/personal-website" exact className="Home" >
                         Home
                     </NavLink>
-                    <NavLink to="/personal-website/about" className="About">
-                        About
-                    </NavLink>
-                    <NavLink to="/personal-website/projects" className="Projects">
-                        Projects
-                    </NavLink>
-                    <NavLink to="/personal-website/contact" className="Contact">
-                        Contact
-                    </NavLink>
-                    
+                </nav>
+                <nav className="Nav-right">
+                        <NavLink to="/personal-website/about" className="About">
+                            About
+                        </NavLink>
+                        <NavLink to="/personal-website/projects" className="Projects">
+                            Projects
+                        </NavLink>
+                        <NavLink to="/personal-website/contact" className="Contact">
+                            Contact
+                        </NavLink>
                 </nav>
             </div>
         </header>
