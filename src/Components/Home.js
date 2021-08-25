386cc7fac6f93d3c65ea3530ccb6299258449b70
xiangxichen11 <xiangxi.chen.ca@gmail.com>
2021-08-25 13:35:47 -0600

    Removed Scripts

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,22 +9,6 @@ export default function Home(){
             <div>
                 <img src="https://st1.bollywoodlife.com/wp-content/uploads/2020/11/Jungkook-BTS.jpg" alt="Jungkook" className="Profile-Photo" />
             </div>
-
-            <div id="vanta-canvas"></div>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.fog.min.js"></script>
-            <script>
-                VANTA.FOG(
-                    el: "#vanta-canvas",
-                    mouseControls: true,
-                    touchControls: true,
-                    gyroControls: false,
-                    minHeight: 200.00,
-                    minWidth: 200.00,
-                    scale: 1.00,
-                    scaleMobile: 1.00
-                )  
-            </script>
             
             <section>
                 <div className="banner">
